36ba2cdf041eff90b1587aa9c3a1b02e4bd3b393
silvabacc <bavernaz@hotmail.com>
2025-05-23 11:50:43 +0100

    add studio button

--- src/components/common/navigation-bar.tsx
+++ src/components/common/navigation-bar.tsx
@@ -14,38 +14,47 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { useTheme } from "next-themes";
 import Image from "next/image";
+import Link from "next/link";
 
 export function NavigationBar() {
   const { setTheme } = useTheme();
 
   return (
-    <div className="flex justify-center m-4">
-      <div className="flex flex-1 justify-center items-center space-x-2">
+    <div className="flex my-4">
+      <Link
+        className="flex flex-1 justify-center items-center ml-24 space-x-2 cursor-pointer"
+        href="/"
+      >
         <Image className="w-12 h-12 rounded" src={TrueIslam} alt="" />
         <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
           True Islam Library
         </h1>
+      </Link>{" "}
+      <div className="mt-2 space-x-2">
+        <Link href={"/studio"}>
+          <Button className="cursor-pointer">Studio</Button>
+        </Link>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="outline" size="icon">
-            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-            <span className="sr-only">Toggle theme</span>
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          <DropdownMenuItem onClick={() => setTheme("light")}>
-            Light
-          </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setTheme("dark")}>
-            Dark
-          </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setTheme("system")}>
-            System
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
     </div>
   );
 }
