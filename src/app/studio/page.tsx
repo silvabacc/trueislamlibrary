2f2a31c4496fad3cf15814438e72852bc915bba4
silvabacc <bavernaz@hotmail.com>
2025-05-23 11:30:05 +0100

    Update page.tsx

--- src/app/studio/page.tsx
+++ src/app/studio/page.tsx
@@ -3,9 +3,10 @@
 import { NextStudio } from "next-sanity/studio";
 import sanityConfig from "@/lib/sanity.config";
 import { useEffect, useState } from "react";
+import { Config } from "sanity";
 
 export default function Studio() {
-  const [config, setConfig] = useState<any>();
+  const [config, setConfig] = useState<Config>({} as Config);
 
   useEffect(() => {
     const getConfig = async () => {
